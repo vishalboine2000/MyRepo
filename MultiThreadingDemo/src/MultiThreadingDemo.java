cac23ccb7a18c755bea32f84f1afc10ba11dbec4
Vishal Boine <vishalboine633@gmail.com>
2021-01-28 14:35:10 +0700

    first commit

---
+++ MultiThreadingDemo/src/MultiThreadingDemo.java
@@ -0,0 +1,31 @@
+public class MultiThreadingDemo extends Thread
+{ 
+	  public void run() 
+	    { 
+	        try
+	        { 
+	            // Displaying the thread that is running 
+	            System.out.println ("Thread " + 
+	                  Thread.currentThread().getId() + 
+	                  " is running"); 
+	  
+	        } 
+	        catch (Exception e) 
+	        { 
+	            // Throwing an exception 
+	            System.out.println ("Exception is caught"); 
+	        } 
+	    }
+    public static void main(String[] args) 
+    { 
+        int n = 8; // Number of threads 
+        for (int i=0; i<n; i++) 
+        { 
+            MultiThreadingDemo object = new MultiThreadingDemo(); 
+            object.start(); 
+        } 
+    } 
+} 
+
+  
+ 
